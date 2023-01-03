a6bd2071fc0200a20f66bcb5b6dd67c85183368e
James Strynkowski <james@titanvest.com>
2023-01-03 09:21:07 -0500

    1.0.13

--- dist/src/getConfig/index.js
+++ dist/src/getConfig/index.js
@@ -1,42 +1,42 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
 exports.getConfig = void 0;
-var fs = require("fs");
-var yup = require("yup");
-var minimist_1 = require("minimist");
-var error = "\nThis package requires the existence of a .gqlTestGenerator.json file, to specify it's purpose.\n\nThis file should be in JSON format, with the following type:\n{\n  schemaFilePath: string; // The path to all schemas for the project.\n  resolverFilePath: string; // The path to the resolver function.\n  resolverName: string; // The name of the resolver function.\n  typeFilePath?: string; // The path to the types file that should be accessed for enums or related typings.\n  maxFixtures?: number; // The maximum number of fixtures/tests that should be generated. Default is 100.\n}";
+var fs = require('fs');
+var yup = require('yup');
+var minimist_1 = require('minimist');
+var error =
+  "\nThis package requires the existence of a .gqlTestGenerator.json file, to specify it's purpose.\n\nThis file should be in JSON format, with the following type:\n{\n  schemaFilePath: string; // The path to all schemas for the project.\n  resolverFilePath: string; // The path to the resolver function.\n  resolverName: string; // The name of the resolver function.\n  typeFilePath?: string; // The path to the types file that should be accessed for enums or related typings.\n  maxFixtures?: number; // The maximum number of fixtures/tests that should be generated. Default is 100.\n}";
 function getError(extension) {
-    return (error +
-        "\n\n".concat(extension, ".\n"));
+  return error + '\n\n'.concat(extension, '.\n');
 }
 function getRequiredString(name) {
-    return yup
-        .string()
-        .min(1, getError("".concat(name, " must be at least 1 character.")))
-        .defined(getError("".concat(name, " is not defined.")))
-        .required(getError("".concat(name, " is required.")));
+  return yup
+    .string()
+    .min(1, getError(''.concat(name, ' must be at least 1 character.')))
+    .defined(getError(''.concat(name, ' is not defined.')))
+    .required(getError(''.concat(name, ' is required.')));
 }
 var configSchema = yup
-    .object({
+  .object({
     resolverName: getRequiredString('resolverName'),
     schemaFilePath: getRequiredString('schemaFilePath'),
     resolverFilePath: getRequiredString('resolverFilePath'),
     typeFilePath: yup.string().min(1).optional(),
-    maxFixtures: yup.number().optional()
-})
-    .defined()
-    .required();
+    maxFixtures: yup.number().optional(),
+  })
+  .defined()
+  .required();
 function getConfig() {
-    var args = (0, minimist_1["default"])(process.argv.slice(2));
-    console.log('args', args);
-    var exists = fs.existsSync('.gqlTestGenerator.json');
-    if (!exists) {
-        throw new Error(error);
-    }
-    // This seems to be expected, but for the life of me, I cannot figure out why.
-    var file = fs.readFileSync('.gqlTestGenerator.json');
-    var config = JSON.parse(file);
-    configSchema.validateSync(config);
-    return config;
+  var args = (0, minimist_1['default'])(process.argv.slice(2));
+  console.log('args', args);
+  var exists = fs.existsSync('.gqlTestGenerator.json');
+  if (!exists) {
+    throw new Error(error);
+  }
+  // This seems to be expected, but for the life of me, I cannot figure out why.
+  var file = fs.readFileSync('.gqlTestGenerator.json');
+  var config = JSON.parse(file);
+  configSchema.validateSync(config);
+  return config;
 }
 exports.getConfig = getConfig;
